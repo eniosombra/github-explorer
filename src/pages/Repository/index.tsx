217647611d7851b7620ecdfd97fdc209e3e88265
Enio L. Sombra <eniosombra@gmail.com>
2020-04-22 11:04:31 -0300

    Conectando a API ao componente Repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 import { Header, RepositoryInfo, Issues } from './styles';
@@ -9,8 +11,61 @@ interface RepositoryParams {
   repository: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
+
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
   const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(response => {
+      setRepository(response.data);
+    });
+
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      setIssues(response.data);
+    });
+  }, [params.repository]);
+
+  /**
+   * Outra forma de utilizar o useEffect onde não importa a ordem das
+   * das respostas provenientes da API
+   *
+    useEffect(() => {
+      async function loadData(): Promise<void> {
+        const [repo, isse] = await Promise.all([
+          api.get(`repos/${params.repository}`),
+          api.get(`repos/${params.repository}/issues`),
+        ]);
+
+        setRepository(repo.data);
+        setIssues(isse.data);
+      }
+
+      loadData();
+    }, [params.repository]);
+   */
 
   return (
     <>
@@ -23,43 +78,49 @@ const Repository: React.FC = () => {
         </Link>
       </Header>
 
-      <RepositoryInfo>
-        <header>
-          <img
-            src="https:\\avatars0.githubusercontent.com/u/28929274?v=4"
-            alt="perfil"
-          />
-          <div>
-            <strong>rocketseat unform</strong>
-            <p>descrição do repositório</p>
-          </div>
-        </header>
+      {repository ? (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
 
-        <ul>
-          <li>
-            <strong>1808</strong>
-            <span>Starts</span>
-          </li>
-          <li>
-            <strong>48</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>67</strong>
-            <span>Issues abertas</span>
-          </li>
-        </ul>
-      </RepositoryInfo>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Starts</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      ) : (
+        <p>Carregando...</p>
+      )}
 
       <Issues>
-        <Link to="teste">
-          <div>
-            <strong>DLJALDJLAF</strong>
-            <p>KASJDLKJADLSF</p>
-          </div>
+        {issues.map(issue => (
+          <a key={issue.id} href={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
 
-          <FiChevronRight size={26} />
-        </Link>
+            <FiChevronRight size={26} />
+          </a>
+        ))}
       </Issues>
     </>
   );
